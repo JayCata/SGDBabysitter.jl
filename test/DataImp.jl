c85e8d1ab1775b758c02aed3e78233876e212d67
JayCata <43505548+JayCata@users.noreply.github.com>
2018-12-14 11:38:32 -0800

    Updated Data Imputation Code and Updated Algorithm with Case 2 and Initialization

--- test/DataImp.jl
+++ test/DataImp.jl
@@ -4,14 +4,15 @@ using Plots
 #set parameters for data generation
 μ=0
 σ=1
-#Sample size and numbe rof ismulations
+#Sample size, number of simulations, number and size of hidden layers
 n=100000
 numsim=10
-nHidden = [3,4,10,12,3,4,5,9,8,7,6,4,3]
+nHidden = [6,15,20,15,6]
 #list for keeping track of simulations
 βlist=[]
 βbiaslist=[]
-βnnlist=[]
+βnnbabysitlist=[]
+βnnvanlist=[]
 misslist=[]
 #Neural Net Parameters and commands
 maxIter = 10000
@@ -30,7 +31,7 @@ X3=X1.^2+X2.^2+ϵ1
 y=2*X1-3*X2+4*X3+ϵ2
 
 
-#Check Consistency
+#Check OLS estimates for all data
 X=hcat(X1,X2,X3)
 β=inv(X'*X)*X'*y
 push!(βlist,β)
@@ -48,17 +49,17 @@ for i in 1:n
 end
 
 #Number of missing
-push!(misslist,n-length(indexl))
-#Show Bias
+push!(misslist,length(indexm))
+
+#Get Bias β
 Xmiss=hcat(X1[indexl],X2[indexl],X3[indexl])
 ymiss=y[indexl]
 βbias=inv(Xmiss'*Xmiss)*Xmiss'*ymiss
 push!(βbiaslist,βbias)
 
-#Fill in Missing Values
-#Shuffle for randomness maybe not necesarry
 
-#Create training and validation sets
+
+#Create training and validation sets (70% training/30% validation)
 numtrain=convert(Int,round(.7*length(indexl)))
 xtrain=hcat(X1[indexl[1:numtrain]],X2[indexl[1:numtrain]])
 x3train=(X3[indexl[1:numtrain]])
@@ -71,23 +72,41 @@ ntrain = size(xtrain,1)
 xtrain = [ones(ntrain,1) xtrain]
 #number of features
 dtrain = size(xtrain,2)
-#number and size of each hidden layer
+
 
 #get number of total weights
 nParams = NeuralNet_nParams(dtrain,nHidden)
-#intialize weight matrix
 
 
 
-#Run neural network to get weights
-w, valid = SGDBabysitter(NeuralNet_backprop, NeuralNet_predict,
-maxIter, nHidden, nParams, xtrain, x3train, xvalid, x3valid)
+#Run neural network to get weights with Vanilla SGD
+w,valid=VanillaSGD(NeuralNet_backprop,NeuralNet_predict,maxIter,nHidden, nParams,xtrain,x3train,xvalid,x3valid, .0001,10)
 
 #predict on missing data
-X3nn=NeuralNet_predict(w,hcat(ones(length(indexm)),X1[indexm],X2[indexm]),nHidden)
-Xnn=vcat(hcat(X1[indexl],X2[indexl],X3[indexl]),hcat(X1[indexm],X2[indexm],X3nn))
-ynn=vcat(y[indexl],y[indexm])
-βnn=inv(Xnn'*Xnn)*Xnn'*ynn
-push!(βnnlist,βnn)
+X3nnvan=NeuralNet_predict(w,hcat(ones(length(indexm)),X1[indexm],X2[indexm]),nHidden)
+#form new X and y
+Xnnvan=vcat(hcat(X1[indexl],X2[indexl],X3[indexl]),hcat(X1[indexm],X2[indexm],X3nnvan))
+ynnvan=vcat(y[indexl],y[indexm])
+
+#Get vanilla SGD OLS estimates
+βnnvan=inv(Xnnvan'*Xnnvan)*Xnnvan'*ynnvan
+push!(βnnvanlist,βnnvan)
+
+#Run neural network to get weights with SGDbabysitter
+w, valid = SGDBabysitter(NeuralNet_backprop, NeuralNet_predict,
+maxIter, nHidden, nParams, xtrain, x3train, xvalid, x3valid,true)
+
+#predict on missing data
+X3nnbabysit=NeuralNet_predict(w,hcat(ones(length(indexm)),X1[indexm],X2[indexm]),nHidden)
+#form new X and y
+Xnnbabysit=vcat(hcat(X1[indexl],X2[indexl],X3[indexl]),hcat(X1[indexm],X2[indexm],X3nnbabysit))
+ynnbabysit=vcat(y[indexl],y[indexm])
+
+#Get SGDBabysitter OLS estimates
+βnnbabysit=inv(Xnnbabysit'*Xnnbabysit)*Xnnbabysit'*ynnbabysit
+push!(βnnbabysitlist,βnnbabysit)
+
+
 end
-print(sum(βbiaslist)/numsim,sum(βlist)/numsim,sum(βnnlist)/numsim,sum(misslist)/numsim, " ")
+#Final report of all OLS estimates
+print("All Data Coefficients: ", sum(βlist)/numsim, " Coefficients with Missing Data: ", sum(βbiaslist)/numsim," Coefficients with NN Imputation (SGDBabysitter): ",sum(βnnbabysitlist)/numsim," Coefficients with NN Imputation (VanillaSGD): " ,sum(βnnvanlist)/numsim," Average Number of Missing Variables: ", sum(misslist)/numsim, " ")
--- test/sgdfunc.jl
+++ test/sgdfunc.jl
@@ -1,52 +1,43 @@
-#include("example_nnet.jl")
 using LinearAlgebra
-function ComputeValid(predict_func::Function, W, xvalid, yvalid, valid)
-    yhat = predict_func(W,[ones(size(xvalid,1)) xvalid],nHidden)
-    push!(valid,sum((yhat-yvalid).^2)/length(yvalid))
+function VanillaSGD(gradcalc::Function, nn_predict::Function, maxIter, nHidden,
+                        nParams, xtrain, ytrain, xvalid, yvalid,a=.0001, B=1)
+    print("Vanilla SGD Running...")                        
+    n = size(xtrain,1)
+    W= randn(nParams,1)
+    flist=[]
+    valid=[]
+    wbest=zeros(nParams)
+    vallow=Inf
+    for t in 1:maxIter
 
-end
+    	i = rand(1:n,B)
+    	f,g = gradcalc(W, xtrain[i,:], ytrain[i], nHidden)
+    	W = W - a*g
 
-function BS_Select(;validation_array,f_array, gradcalc::Function,xtrain,ytrain,W,nHidden, alist, Blist)
-    #Get most recent step and batch sizes
-    a=alist[end]
-    B=Blist[end]
-    n,d=size(xtrain)
-
-    #Get Gradient Angle
-    i=rand(1:n,B)
-    j=rand(1:n,B)
-    _,gi=gradcalc(W, xtrain[i,:],ytrain[i],nHidden)
-    _,gj=gradcalc(W,xtrain[j,:],ytrain[j], nHidden)
-    costheta=dot(gi,gj)/(norm(gi)*norm(gj))
-    #theta=acos(costheta)
-
-
-
-    #alpha decreases
-    if length(validation_array) >= 4 && validation_array[end-2]>validation_array[end]
-        a_adjust = (1 - abs(validation_array[end-2] - validation_array[end])/validation_array[end-2])^(1/4)
-        a = a*a_adjust
+        if (mod(t-1,round(maxIter/100)) == 0)
+            # print("Training iteration = $(t-1) ")
+            push!(flist,f)
+            ComputeValid(nn_predict, W, xvalid, yvalid, valid)
+            if valid[end]<vallow
+                wbest=W
+            end
+        end
     end
-    push!(alist,a)
-    push!(Blist,B)
-    print(a," ")
-
-
-    return a,B
+    display(plot(1:length(valid), valid))
+    return wbest, valid
 end
 
 function SGDBabysitter(gradcalc::Function, nn_predict::Function, maxIter, nHidden,
-                        nParams, xtrain, ytrain, xvalid, yvalid )
-
+                        nParams, xtrain, ytrain, xvalid, yvalid, dyn=true)
+    @assert(dyn == true || dyn==false)
     n = size(xtrain,1)
     valid=[]
     flist=[]
     alist=[]
     Blist=[]
 
-    #placehold for initialization
-    B=100
-    a = 1e-4
+    a,B=BS_initialize(gradcalc, nn_predict, maxIter, nHidden,
+                            nParams, xtrain, ytrain, xvalid, yvalid)
     push!(Blist,B)
     push!(alist,a)
     W= randn(nParams,1)
@@ -68,7 +59,7 @@ function SGDBabysitter(gradcalc::Function, nn_predict::Function, maxIter, nHidde
                   wbest=W
               end
           end
-        if (mod(t-1,round(maxIter/50)) == 0)
+        if (mod(t-1,round(maxIter/50)) == 0) && dyn==1
             a,B=BS_Select(validation_array=valid,f_array=flist, gradcalc=gradcalc,xtrain=xtrain,ytrain=ytrain,W=W,nHidden=nHidden, alist=alist, Blist=Blist)
 
           end
@@ -78,3 +69,69 @@ function SGDBabysitter(gradcalc::Function, nn_predict::Function, maxIter, nHidde
 
     return wbest, valid
 end
+
+
+
+#INITIALIZATION FUNCTION--------------------------------------------------------
+function BS_initialize(gradcalc::Function, nn_predict::Function, maxIter, nHidden,
+                        nParams, xtrain, ytrain, xvalid, yvalid)
+switch=1
+n = size(xtrain,1)
+B=5
+a=.999999999
+j=1
+while switch==1 && j<10
+    W= randn(nParams,1)
+    flist=[]
+    validlist=[]
+for t in 1:10
+    i = rand(1:n,B)
+    f,g = gradcalc(W, xtrain[i,:], ytrain[i], nHidden)
+    W = W - a*g
+    push!(flist,f)
+    ComputeValid(nn_predict, W, xvalid, yvalid, validlist)
+end
+if validlist[end]<(4/6)*validlist[1] && flist[end]<(4/5)flist[1]
+    switch=0
+else
+    a=(6/10)*a
+    print("Initializing... ")
+    j+=1
+end
+end
+return a,B
+end
+#Selection Function-------------------------------------------------------------
+function BS_Select(;validation_array,f_array, gradcalc::Function,xtrain,ytrain,W,nHidden, alist, Blist)
+    #Get most recent step and batch sizes
+    a=alist[end]
+    B=Blist[end]
+    n,d=size(xtrain)
+
+    #Get Gradient Angle
+    i=rand(1:n,B)
+    j=rand(1:n,B)
+    _,gi=gradcalc(W, xtrain[i,:],ytrain[i],nHidden)
+    _,gj=gradcalc(W,xtrain[j,:],ytrain[j], nHidden)
+    costheta=dot(gi,gj)/(norm(gi)*norm(gj))
+    #theta=acos(costheta)
+
+
+
+    #case 1 alpha decreases based on validation error
+    if length(validation_array) >= 4 && validation_array[end-2]>validation_array[end]
+        a_adjust = (1 - abs(validation_array[end-2] - validation_array[end])/validation_array[end-2])^2
+        a = a*a_adjust
+    end
+
+    #case 2 alpha decrease if validation error does not
+    if length(validation_array) >= 4 && (validation_array[end-4] < validation_array[end]||validation_array[end-3] < validation_array[end]) && (validation_array[end-2]<validation_array[end])
+        a=(3/4)*a
+    end
+
+    push!(alist,a)
+    push!(Blist,B)
+    print(a," ")
+    return a,B
+end
+#Compute Validation-------------------------------------------------------------
